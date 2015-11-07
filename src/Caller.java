db49abf228912c1be6aa08b6ac4ed4138d566096
TkachenkoMax <manutd20x@gmail.com>
2015-11-07 18:29:47 +0200

    Caller ver2

--- src/Caller.java
+++ src/Caller.java
@@ -1,32 +1,55 @@
 import java.io.IOException;
 import java.net.SocketAddress;
+import java.util.StringJoiner;
 
 public class Caller {
 
     private String localNick;
     private SocketAddress remoteAddress;
     private String remoteNick;
+    private String ip;
+
+    public static enum CallStatus {
+        BUSY, NO_SERVICE, NOT_ACCESSIBLE, OK, REJECTED
+    }
+
+    public static CallStatus[] values() {
+        return CallStatus.values();
+    }
+
+    public static CallStatus valueOf(String name) throws IllegalArgumentException, NullPointerException {
+        return CallStatus.valueOf(name);
+    }
 
     public Caller() {
-
+        localNick = "username1";
+        remoteNick = "username2";
+        ip = "8.8.4.4";
     }
 
     public Caller(String localNick) {
-
+        this.localNick = localNick;
+        remoteNick = "username2";
+        ip = "8.8.4.4";
     }
 
     public Caller(String localNick, SocketAddress remoteAddress) {
-
+        this.localNick = localNick;
+        remoteNick = "username2";
+        ip = "8.8.4.4";
+        this.remoteAddress = remoteAddress;
     }
 
     public Caller(String localNick, String ip) {
-
+        this.localNick = localNick;
+        this.ip = ip;
+        remoteNick = "username2";
     }
 
-  /*  public Connection call() throws IOException{
-
+    public Connection call() throws IOException {
+        return null;
     }
-*/
+
 
     public String getLicalNick() {
         return localNick;
@@ -46,6 +69,10 @@ public class Caller {
 
     public void setLocalNick(String localNick) {
         this.localNick = localNick;
+    }
+
+    public CallStatus getStatus() {
+        return null;
     }
 
     @Override
